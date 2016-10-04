90f4d92de0e2820fd98e52ad88c210079148a10a
Tami <catquarks@gmail.com>
2016-10-04 15:29:57 -0400

    define actions, iterate through interests, format checkboxes

--- src/components/search_box.js
+++ src/components/search_box.js
@@ -1,33 +1,52 @@
 import React from 'react'
-// import * as actions from '../actions'
+import * as actions from '../actions'
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 
 
-export default class SearchBox extends React.Component{
+class SearchBox extends React.Component{
 	constructor(props){
 		super(props)
+		this.state = {
+			selectedInterests: []
+		}
 		this.checkboxHandler = this.checkboxHandler.bind(this)
-
 	}
 
 	checkboxHandler (event){
-		
-		
+		const interests = this.state.selectedInterests
+		if (event.target.checked){
+			const newInterest = this.props.interests.filter((interest) => {
+				return (interest.id === parseInt(event.target.id))
+			})
+
+			this.setState({
+				selectedInterests: [...interests, newInterest]
+			})
+
+			actions.fetchFields(slfjds)
+			debugger
+		} else {
+			debugger
+		}
+
 		debugger
 	}
-
-
 
 	render(){
 		return(
 			<div>
 				<h1>Search Your Interests</h1>
 				<form>
-					<input type="checkbox" id="checkbox" onChange={this.checkboxHandler} />
-					<label htmlFor="checkbox">Check!</label>
-					<input type="checkbox" id="checkbox2" ref="huh" onChange={this.checkboxHandler} />
-					<label htmlFor="checkbox2">Check2!</label>				
+					{this.props.interests.map((interest) => {
+						return(
+							<div key={interest.id}>
+								<input type="checkbox" id={interest.id} onChange={this.checkboxHandler} ref={interest.id} />
+								<label htmlFor={interest.id}> {interest.name}</label>
+							</div>
+						)
+					})}
+	
 				</form>
 			</div>
 		)
@@ -35,8 +54,9 @@ export default class SearchBox extends React.Component{
 }
 
 function MapStateToProps(state){
-	debugger
-
+	return{
+		interests: state.interests
+	}
 }
 
-// export default connect(MapStateToProps)(SearchBox)
+export default connect(MapStateToProps)(SearchBox)
