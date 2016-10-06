306b4f7d3f8e5207d04783cc9d1bfdf79f52ecb5
Abigail Diaz <github email address>
2016-10-06 11:41:16 -0400

    Added notes for Quiz questions (in Components-Quiz.js)

---
+++ src/components/quiz.js
@@ -0,0 +1,85 @@
+/*
+
+Uses state to keep track of points for each field per Interest answers provided by the User.
+
+class Quiz extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      science: 0,
+      art: 0,
+      music: 0,
+      education: 0,
+      business: 0,
+      tech: 0,
+      hospitality: 0,
+      healthcare: 0,
+      editorial: 0,
+      engineering: 0,
+      law: 0,
+      trade: 0
+    }
+  }
+}
+
+Questions:
+1. What would you rather do on a free Saturday?
+  a) Fixing people (--> healthcare, science, education)
+  b) Shopping at thrift stores (--> art, music)
+  c) Building your own custom computer (--> tech, engineering, science, trade)
+  d) Enjoying fine wines (--> hospitality, music, editorial, law, business)
+
+2. Pick the image that most excites you:
+  1) Picture of money (--> Business, Law)
+  2) Picture of kittens next to books (--> science, editorial, art, medicine, education)
+  3) Picture of A lot of FOOD! (--> hospitality)
+  4) Picture of welding (--> trade, tech, engineering)
+
+3. If you saw a kitten stuck in a tree how would you help it?
+  a) Call for help (--> editorial, education, hospitality, healthcare)
+  b) Build a ladder to climb up the tree (--> science, engineering, tech)
+  c) Suit-up! Climb the tree with your bare hands (--> trade, art, music)
+  d) Ignore the kitten, they always land on their feet (--> business, law)
+
+4. Which of these sentiments do you agree with most?
+  a) "What we do every night, Pinky. Try to take over the world" --Brain (--> business, science, law)
+  b) "Hakuna Matata" --Puumba and Timon (--> art, music)
+  c) "Guard well within yourself that treasure, kindness. Know how to give without hesitation, how to lose without regret, how to acquire without meanness." -George Sand (--> hospitality, healthcare, editorial, art, education)
+  d) "Computers make excellent and efficient servants but I have no wish to serve under them." --Mr. Spock (--> science, tech, engineering, trade)
+
+5. Rate how strongly you agree with the following statement.
+  "I always tip at least 20% because everyone deserves a living wage"
+  Strongly Disagree (--> law)
+  Disagree (--> business)
+  Neutral (--> trade, science, tech, engineering)
+  Agree (--> editorial, education, healthcare)
+  Strongy Agree (--> hospitality, art, music)
+
+6. Pick the character you most identify with:
+  1) Ariel (--> music)
+  2) Gaston (--> law)
+  3) Beaker (--> science)
+  4) Pocahontas (--> art)
+  5) Tiana (--> hospitality)
+  6) Gepetto (--> trade)
+  7) Wall-e (--> tech)
+  8) Belle (--> editorial)
+  9) Scrooge McDuck (--> business)
+  10) Lego Batman (--> engineering)
+  11) Rafiki (--> healthcare)
+  12) Yoda (--> education)
+
+7. What's your preferred color?
+  red (--> art, music, editorial, business)
+  yellow (--> hospitality, education, law)
+  blue (--> healthcare, trade, science, engineering, tech)
+
+8. True or False: You like to work from home.
+  True: (--> business, art, music, editorial, tech)
+  False: (--> hospitality, science, law, healthcare, trade, engineering, education)
+
+9. What does this look like to you?
+[Image of Rorschach test]
+Text box --> make it funny
+
+*/
